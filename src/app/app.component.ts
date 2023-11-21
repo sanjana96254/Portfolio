41109fc61edbd74c1404f2bdece1bc319bcd1576
sanjana96254 <86880559+sanjana96254@users.noreply.github.com>
2023-11-21 13:45:31 +0530

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,15 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Portfolio';
-  data ='Sanjana Agrawal'
+  data = "Sanjana Agrawal"
 }
